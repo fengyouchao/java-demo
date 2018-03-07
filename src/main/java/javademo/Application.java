51c5b86c12413bf7e988b04e5f7343ad88830830
Youchao Feng <fengyouchao@gmail.com>
2018-03-07 12:56:53 +0800

    first commit

---
+++ src/main/java/javademo/Application.java
@@ -0,0 +1,12 @@
+package javademo;
+
+/**
+ * Created on 2018/3/7.
+ */
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+
+}
---
+++ src/test/java/javademo/test/TestApplication.java
@@ -0,0 +1,16 @@
+package javademo.test;
+
+import org.junit.Test;
+
+/**
+ * Created on 2018/3/7.
+ *
+ */
+public class TestApplication {
+
+  @Test
+  public void test(){
+    System.out.println("test hello");
+  }
+
+}
